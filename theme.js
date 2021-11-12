1f84659a6c843cb86b3765d88f71aa19a9ae1d2a
Garrett Long <garrett.long.ext@bayer.com>
2021-11-12 11:35:31 -0600

    added initial theme

---
+++ theme.js
@@ -0,0 +1,43 @@
+class NordAlpineTheme {
+  initialize(ctx) {
+    ctx.theme.add("nordAlpine", {
+      colors: {
+        primary: "#D08770",
+        secondary: "#BF616A",
+        bg: "#2E3440",
+        offBg: "#3B4252",
+        font: "#E5E9F0",
+        offFont: "#D8DEE9",
+        border: "#E5E9F0",
+        offBorder: "#D8DEE9",
+        headerBg: "#2E3440",
+      },
+      shadow: {
+        color: "#434C5E",
+        opacity: 0.1,
+      },
+      editor: {
+        colors: {
+          comment: "#4C566A",
+          string: "#A3BE8C",
+          number: "#B48EAD",
+          variable: "#EBCB8B",
+          keyword: "#B48EAD",
+          atom: "#BF616A",
+          attribute: "#EBCB8B",
+          property: "#D8DEE9",
+          punctuation: "#B48EAD",
+          definition: "#EBCB8B",
+          builtin: "#D08770",
+          cursor: "#D8DEE9",
+        },
+      },
+    });
+
+    ctx.theme.enable("nordAlpine");
+  }
+
+  async destroy() {}
+}
+
+window.AltairGraphQL.plugins.NordTheme = NordAlpineTheme;
